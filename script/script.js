75b86f10182825688f9bca0ddd7e2f94a79a9614
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-05-12 16:56:59 +0200

    Working on the skill gauge

--- script/script.js
+++ script/script.js
@@ -7,6 +7,12 @@ let columnProject = document.querySelectorAll(".columnproject");
 let competenceOpen = document.getElementById("competence-open");
 let competenceOverlay = document.getElementById("competence-overlay");
 let closeCompetence = document.getElementById("closecompetence");
+let circles= document.querySelectorAll(".circles");
+let level= document.querySelectorAll(".level");
+let text= document.querySelectorAll(".text");
+let percent= document.querySelectorAll(".percent");
+let number= document.querySelectorAll(".number");
+let skillCard= document.querySelectorAll(".card");
 
 
 console.log(projectsOpen);
@@ -31,12 +37,47 @@ closeOverlay.addEventListener("click", function() {
 
 competenceOpen.addEventListener("click", function() {
   competenceOverlay.classList.remove("hidden");
+  for (const sc of skillCard){
+    sc.classList.remove("hidden");
+    }
+  for (const c of circles){
+    c.classList.remove("hidden");
+    }
+  for (const l of level){
+    l.classList.remove("hidden");
+    }
+  for (const t of text){
+    t.classList.remove("hidden");
+    }
+  for (const p of percent){
+    p.classList.remove("hidden");
+    }
+  for (const n of number){
+    n.classList.remove("hidden");
+    }
 })
 
 closecompetence.addEventListener("click", function() {
    
   competenceOverlay.classList.add("hidden");
-
+  for (const sc of skillCard){
+    sc.classList.add("hidden");
+    }
+  for (const c of circles){
+    c.classList.add("hidden");
+    }
+  for (const l of level){
+      l.classList.add("hidden");
+    }
+  for (const t of text){
+      t.classList.add("hidden");
+    }
+    for (const p of percent){
+      p.classList.add("hidden");
+      }
+    for (const n of number){
+      n.classList.add("hidden");
+      }
 })
 
 
