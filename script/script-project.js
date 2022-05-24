1a29e07231d882eb5b383590a1792759a4ab83d0
RenaudJean <95225657+Renaudjean@users.noreply.github.com>
2022-05-24 14:11:50 +0200

    Finsihed the responsive design

---
+++ script/script-project.js
@@ -0,0 +1,83 @@
+let projectsOpen = document.getElementById("projectOpen");
+let projectOverlay = document.getElementById("projectOverlay");
+let closeOverlay = document.getElementById("closeoverlay");
+let columnProject = document.querySelectorAll(".columnproject");
+
+let competenceOpen = document.getElementById("competence-open");
+let competenceOverlay = document.getElementById("competence-overlay");
+let closeCompetence = document.getElementById("closecompetence");
+let circles= document.querySelectorAll(".circles");
+let level= document.querySelectorAll(".level");
+let text= document.querySelectorAll(".text");
+let percent= document.querySelectorAll(".percent");
+let number= document.querySelectorAll(".number");
+let skillCard= document.querySelectorAll(".card");
+let paused= document.querySelectorAll(".paused");
+
+
+
+projectsOpen.addEventListener("click", function() {
+    projectsOverlay.classList.remove("hidden");
+    for (const columnP of columnProject){
+      console.log(columnP);
+
+      columnP.classList.remove("hidden");
+      }
+  } )
+
+closeOverlay.addEventListener("click", function() {
+   
+    projectsOverlay.classList.add("hidden");
+     for (const columnP of columnProject){
+      columnP.classList.add("hidden");
+    }
+  } )
+
+competenceOpen.addEventListener("click", function() {
+  competenceOverlay.classList.remove("hidden");
+  for (const sc of skillCard){
+    sc.classList.remove("hidden");
+    }
+  for (const c of circles){
+    c.classList.remove("hidden");
+    }
+  for (const l of level){
+    l.classList.remove("hidden");
+    }
+  for (const t of text){
+    t.classList.remove("hidden");
+    }
+  for (const p of percent){
+    p.classList.remove("hidden");
+    }
+  for (const n of number){
+    n.classList.remove("hidden");
+    }
+  for (const p of paused){
+    p.classList.remove("paused");
+    }
+})
+
+closecompetence.addEventListener("click", function() {
+   
+  competenceOverlay.classList.add("hidden");
+  for (const sc of skillCard){
+    sc.classList.add("hidden");
+    }
+  for (const c of circles){
+    c.classList.add("hidden");
+    }
+  for (const l of level){
+      l.classList.add("hidden");
+    }
+  for (const t of text){
+      t.classList.add("hidden");
+    }
+    for (const p of percent){
+      p.classList.add("hidden");
+      }
+    for (const n of number){
+      n.classList.add("hidden");
+      }
+})
+
